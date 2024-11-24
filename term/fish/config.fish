c6552460287066b6e101ad069f3b0da5927f6947
rReyvn <reyvn029@gmail.com>
2024-11-24 21:40:46 +0700

    Dots: Update completion

--- term/fish/config.fish
+++ term/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
 	set -U fish_greeting ""
   set -gx TERM xterm-256color
   set -gx EDITOR nvim
+  set -gx BROWSER zen-browser
   
   fish_add_path ~/.local/bin
   fish_add_path ~/.cargo/bin/
